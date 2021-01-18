e223193a7ea8053abd142e2f414c523389c72f1e
adamhutchings <adam.abahot@gmail.com>
2021-01-18 15:33:42 -0800

    Create main method and utils

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * Main driver code for the fabric interpreter, compiles the source file into
+ * an abstract representation and executes it until stopped or the end of 
+ * bytecodes is reached.
+ */
+
+/**
+ * Main function for Fabric.
+ * @param argc the argument count
+ * @param argv the arguments themselves
+ * @return success code of the program itself
+ * @author Adam Hutchings
+ */
+int main(int argc, char** argv) {
+
+}
---
+++ src/util.h
@@ -0,0 +1,61 @@
+/**
+ * General utilities for string checking, assertions, etc.
+ */
+
+#ifndef FC_UTIL_H
+#define FC_UTIL_H
+
+#include <stdio.h> /* fprintf, stderr */
+#include <stdlib.h> /* exit */
+
+/**
+ * General error codes.
+ */
+typedef enum {
+
+    FC_SUCCESS, /* Everything was fine. */
+    FC_FAILED,  /* Some unknown error. */
+    FC_NULLPTR, /* A null pointer was passed in. */
+    FC_MEM,     /* A malloc failed. */
+
+} fc_status_t;
+
+/**
+ * Exit with an error message.
+ * @param msg the format (does not need a newline at the end)
+ * @param ... format parameters
+ */
+#define FC_ERROR(msg, ...)                                        \
+    do {                                                          \
+        fprintf(stderr, "Error: fabric: " msg "\n", __VA_ARGS__); \
+        exit(-FC_FAILED);                                         \
+    } while (0)
+
+
+/**
+ * Display a warning but keep execution going.
+ * @param msg the format (does not need a newline at the end)
+ * @param ... format parameters
+ */
+#define FC_WARN(msg, ...)                                           \
+    do {                                                            \
+        fprintf(stderr, "Warning: fabric: " msg "\n", __VA_ARGS__); \
+    } while (0)
+
+/**
+ * Make sure that something is true. If not, exit with an error.
+ */
+#define FC_ASSERT(cond)                          \
+    do {                                         \
+        if (!(cond)) {                           \
+            FC_ERROR(                            \
+                "assertion failed:"              \
+                "File \"%s\", function \"%s\", " \
+                "line %d:\n\t%s",                \
+                __FILE__, __FUNC__, __LINE__,    \
+                #cond                            \
+            );                                   \
+        }                                        \
+    } while (0)
+
+#endif /* FC_UTIL_H */
